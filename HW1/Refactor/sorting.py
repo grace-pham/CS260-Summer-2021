f561d26fc1fd9d8a8068bbe6fb60206b83cf8a95
grace-pham <gracepham208@gmail.com>
2021-06-26 02:07:00 +0700

    fix: Create function plot_result to enhance code reusability

--- HW1/Refactor/sorting.py
+++ HW1/Refactor/sorting.py
@@ -8,47 +8,58 @@ Instruction on installing matplotlib library:
 
 from result_table import *
 
-# Plot results list using pandas pivot_table
-table = df.pivot_table(index=x_list, values=df.columns).plot()
-plt.title("Table 0")
-plt.show()
+
+# Function for plotting the function result
+def plot_result(index_list, value_list, table_title):
+    table = df.pivot_table(index=index_list, values=value_list).plot()
+    plt.title(table_title)
+    plt.show()
+
+
+# Plot result lists of all 12 functions using pandas pivot_table
+plot_result(index_list=x_list, value_list=df.columns, table_title="Table 0")
+
 
 # f4_result line grows the fastest compared to other lines, so we exclude f4 out of the values for the plot
 # Plot the remaining result lists
-table1 = df.pivot_table(index=x_list,
-                        values=['f1', 'f2', 'f3', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10', 'f11', 'f12']).plot()
-plt.title("Table 1")
-plt.show()
+plot_result(index_list=x_list,
+            value_list=['f1', 'f2', 'f3', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10', 'f11', 'f12'],
+            table_title="Table 1")
+
 
 # f1_result line grows the fastest compared to other lines, so we exclude f1 out of the values for the plot
 # Plot the remaining result lists
-table2 = df.pivot_table(index=x_list, values=['f2', 'f3', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10', 'f11', 'f12']).plot()
-plt.title("Table 2")
-plt.show()
+plot_result(index_list=x_list,
+            value_list=['f2', 'f3', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10', 'f11', 'f12'],
+            table_title="Table 2")
+
 
 # f9_result line grows the fastest compared to other lines, so we exclude f9 out of the values for the plot
 # Plot the remaining result lists
-table3 = df.pivot_table(index=x_list, values=['f2', 'f3', 'f5', 'f6', 'f7', 'f8', 'f10', 'f11', 'f12']).plot()
-plt.title("Table 3")
-plt.show()
+plot_result(index_list=x_list,
+            value_list=['f2', 'f3', 'f5', 'f6', 'f7', 'f8', 'f10', 'f11', 'f12'],
+            table_title="Table 3")
+
 
 # f3_result line grows the fastest compared to other lines, so we exclude f3 out of the values for the plot
 # Plot the remaining result lists
-table4 = df.pivot_table(index=x_list, values=['f2', 'f5', 'f6', 'f7', 'f8', 'f10', 'f11', 'f12']).plot()
-plt.title("Table 4")
-plt.show()
+plot_result(index_list=x_list,
+            value_list=['f2', 'f5', 'f6', 'f7', 'f8', 'f10', 'f11', 'f12'],
+            table_title="Table 4")
+
 
 # f10_result line grows the fastest compared to other lines, so we exclude f10 out of the values for the plot
 # Plot the remaining result lists
-table5 = df.pivot_table(index=x_list, values=['f2', 'f5', 'f6', 'f7', 'f8', 'f11', 'f12']).plot()
-plt.title("Table 5")
-plt.show()
+plot_result(index_list=x_list,
+            value_list=['f2', 'f5', 'f6', 'f7', 'f8', 'f11', 'f12'],
+            table_title="Table 5")
+
 
 # f11_result line grows the fastest compared to other lines, so we exclude f11 out of the values for the plot
 # Plot the remaining result lists
-table6 = df.pivot_table(index=x_list, values=['f2', 'f5', 'f6', 'f7', 'f8', 'f12']).plot()
-plt.title("Table 6")
-plt.show()
+plot_result(index_list=x_list,
+            value_list=['f2', 'f5', 'f6', 'f7', 'f8', 'f12'],
+            table_title="Table 6")
 
 # From the plot result in Table 6, we can conclude the order of growth rate in ascending order is f12, f8, f2, f7, f5
 
