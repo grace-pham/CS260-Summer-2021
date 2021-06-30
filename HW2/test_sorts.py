912a2cbe7ec4a4457657ca06f8c9b1e6176b618a
grace-pham <gracepham208@gmail.com>
2021-06-30 23:12:18 +0700

    fix: Refine codes with PEP8

--- HW2/test_sorts.py
+++ HW2/test_sorts.py
@@ -1,44 +1,61 @@
-#Mark Boady
-#Drexel University
-#CS 260
+# Mark Boady
+# Drexel University
+# CS 260
 
-#This function generate a random list of
-#size
-#It sorts using the given sort_fun
-#It then checks that the answer is correct
+# This function generate a random list of
+# size
+# It sorts using the given sort_fun
+# It then checks that the answer is correct
 import random
-def check_list(sort_fun,size):
-	X=[random.randint(-10*size,10*size) for n in range(0,size)]
-	T=X[:]#Make a copy
-	sort_fun(T)
-	#Is the array sorted?
-	for i in range(1,len(T)):
-		assert T[i] >= T[i-1]
-	#Are all the elements in the list?
-	for num in X:
-		assert T.count(num)==X.count(num)
-	return
-	
+
+
+def check_list(sort_fun, size):
+    X = [random.randint(-10 * size, 10 * size) for n in range(0, size)]
+    T = X[:]  # Make a copy
+    sort_fun(T)
+    # Is the array sorted?
+    for i in range(1, len(T)):
+        assert T[i] >= T[i - 1]
+    # Are all the elements in the list?
+    for num in X:
+        assert T.count(num) == X.count(num)
+    return
+
+
 import sort_code
 
-#Built In Sort Tests
+
+# Built In Sort Tests
 def test_builtin_ten():
-	check_list(sort_code.builtin,10)
+    check_list(sort_code.builtin, 10)
+
+
 def test_builtin_hundred():
-	check_list(sort_code.builtin,100)
+    check_list(sort_code.builtin, 100)
+
+
 def test_builtin_thousand():
-	check_list(sort_code.builtin,1000)
+    check_list(sort_code.builtin, 1000)
+
+
 def test_builtin_fivethousand():
-	check_list(sort_code.builtin,5000)
+    check_list(sort_code.builtin, 5000)
 
-#Gnome SORT Tests
+
+# Gnome SORT Tests
 def test_gnome_ten():
-	check_list(sort_code.gnome,10)
-def test_gnome_hundred():
-	check_list(sort_code.gnome,100)
-def test_gnome_thousand():
-	check_list(sort_code.gnome,1000)
-def test_gnome_fivethousand():
-	check_list(sort_code.gnome,5000)
+    check_list(sort_code.gnome, 10)
 
-#Add your own tests for the new sorts below
+
+def test_gnome_hundred():
+    check_list(sort_code.gnome, 100)
+
+
+def test_gnome_thousand():
+    check_list(sort_code.gnome, 1000)
+
+
+def test_gnome_fivethousand():
+    check_list(sort_code.gnome, 5000)
+
+# Add your own tests for the new sorts below
--- HW2/time_sorts.py
+++ HW2/time_sorts.py
@@ -1,36 +1,41 @@
-#Mark Boady
-#Drexel University
-#CS 260
+# Mark Boady
+# Drexel University
+# CS 260
 
-#This is just a starting point.
-#It times the first two sorts.
+# This is just a starting point.
+# It times the first two sorts.
 
-#Extend this code to handle the 4 new sorts.
-#Some sorts will take to long and you will
-#have to stop them earlier than others.
+# Extend this code to handle the 4 new sorts.
+# Some sorts will take to long and you will
+# have to stop them earlier than others.
 
-#Time Two Sorts
+# Time Two Sorts
 import random
 import timeit
-#Generate a Random Array of Numbers to sort
-def rand_array(size):
-	return [random.randint(-2*size,2*size) for n in range(0,size)]
-#Test the given sort on a random array of size given.
-def test_a_sort(sort_name,size):
-	setup="from sort_code import "+sort_name+"\n"
-	setup+="from __main__ import rand_array\n"
-	setup+="size="+str(size)+"\n"
-	setup+="A=rand_array(size)\n"
-	test_code=sort_name+"(A.copy())"
-    #You might need to change these parameters
-	t=timeit.timeit(test_code,setup,number=100)
-	return t
 
-if __name__=="__main__":
-    #Print out the results.
+
+# Generate a Random Array of Numbers to sort
+def rand_array(size):
+    return [random.randint(-2 * size, 2 * size) for n in range(0, size)]
+
+
+# Test the given sort on a random array of size given.
+def test_a_sort(sort_name, size):
+    setup = "from sort_code import " + sort_name + "\n"
+    setup += "from __main__ import rand_array\n"
+    setup += "size=" + str(size) + "\n"
+    setup += "A=rand_array(size)\n"
+    test_code = sort_name + "(A.copy())"
+    # You might need to change these parameters
+    t = timeit.timeit(test_code, setup, number=100)
+    return t
+
+
+if __name__ == "__main__":
+    # Print out the results.
     print("Size | Built In    |    Gnome")
-    for x in range(0,9):
-        rowTemplate="{:4d} | {:11.5} | {:11.5f}"
-        built=test_a_sort("builtin",2**x)
-        gnome=test_a_sort("gnome",2**x)
-        print(rowTemplate.format(2**x,built,gnome))
+    for x in range(0, 9):
+        rowTemplate = "{:4d} | {:11.5} | {:11.5f}"
+        built = test_a_sort("builtin", 2 ** x)
+        gnome = test_a_sort("gnome", 2 ** x)
+        print(rowTemplate.format(2 ** x, built, gnome))
