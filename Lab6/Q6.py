3f632088005619e5777a93bf760a3c2c938f4c6b
Grace Pham <gracepham208@gmail.com>
2021-08-02 13:27:45 -0400

    fix: Add implementation for heap_sort function

--- Lab6/Q6.py
+++ Lab6/Q6.py
@@ -1,11 +1,28 @@
 import heapq
 
-my_heap = []
 
-heapq.heappush(my_heap, (7, "Process 3"))
-heapq.heappush(my_heap, (2, "Process 9"))
-heapq.heappush(my_heap, (0, "OS Task"))
+def instruction():
+    my_heap = []
 
-print(f"The first task to do is: {heapq.heappop(my_heap)}")
-print(f"The second task to do is: {heapq.heappop(my_heap)}")
-print(f"The third task to do is: {heapq.heappop(my_heap)}")
+    heapq.heappush(my_heap, (7, "Process 3"))
+    heapq.heappush(my_heap, (2, "Process 9"))
+    heapq.heappush(my_heap, (0, "OS Task"))
+
+    print(f"The first task to do is: {heapq.heappop(my_heap)}")
+    print(f"The second task to do is: {heapq.heappop(my_heap)}")
+    print(f"The third task to do is: {heapq.heappop(my_heap)}")
+
+
+def heap_sort(arr):
+    P = []
+    size = len(arr)
+    for i in range(size):
+        heapq.heappush(P, arr[i])
+    for i in range(size):
+        arr[i] = heapq.heappop(P)
+
+
+if __name__ == "__main__":
+    test_list_1 = [9, 2, 4, 1, 8, 5, 3]
+    heap_sort(test_list_1)
+    print(test_list_1)
