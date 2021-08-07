0cdbb6d41e5678598a533ed32edd06b7f10bc5fb
Grace Pham <gracepham208@gmail.com>
2021-08-07 12:35:54 -0400

    feat: Add time_sort and sort_code py files

---
+++ HW/HW6/sort_code.py
@@ -0,0 +1,129 @@
+# Mark Boady - CS 260
+# Drexel University
+
+# Implement the sorts from lecture in this file.
+
+# Here are two example sorts to start off with.
+
+# These functions have no return values.
+# They sort the array in place.
+
+# The built in sort is probably optimal.
+def builtin(A):
+    A.sort()
+    return
+
+
+# Gnome sort is pretty weird.
+# It is like a bad version of insertion sort
+# Read more:https://en.wikipedia.org/wiki/Gnome_sort
+def gnome(A):
+    # Current index in the array
+    i = 0
+    # While we haven't made it to the end
+    while i < len(A):
+        # We cannot compare at 0
+        # Otherwise check order is correct
+        if i == 0 or A[i] >= A[i - 1]:
+            i += 1  # Move forward
+        else:
+            # Swap values
+            temp = A[i]
+            A[i] = A[i - 1]
+            A[i - 1] = temp
+            # Go back one and try again
+            i -= 1
+    return
+
+
+# Implement the sorts from class below!
+
+def swap(swapped_list, index_a, index_b):
+    temp = swapped_list[index_a]
+    swapped_list[index_a] = swapped_list[index_b]
+    swapped_list[index_b] = temp
+
+
+def partition(input_list, start, stop):
+    pivot = input_list[stop]
+    i = start
+    for j in range(start, stop):
+        if input_list[j] <= pivot:
+            swap(input_list, i, j)
+            i += 1
+    swap(input_list, i, stop)
+    return i
+
+
+def bubble_sort(input_list):
+    swapped = 1
+    while swapped == 1:
+        swapped = 0
+        i = 1
+        while i < len(input_list):
+            if input_list[i - 1] > input_list[i]:
+                swap(input_list, i - 1, i)
+                swapped = 1
+            i += 1
+
+
+def insertion_sort(input_list):
+    i = 1
+    while i < len(input_list):
+        j = i
+        while j > 0 and input_list[j - 1] > input_list[j]:
+            swap(input_list, j - 1, j)
+            j = j - 1
+        i += 1
+
+
+def merge(input_list, start, middle, stop):
+    aux_list = []
+
+    for input_value in input_list[start:stop + 1]:
+        aux_list.append(input_value)
+
+    aux_middle = middle - start
+    aux_stop = stop - start
+
+    i = 0
+    j = aux_middle + 1
+
+    for k in range(start, stop + 1):
+        if i > aux_middle:
+            input_list[k] = aux_list[j]
+            j += 1
+        elif j > aux_stop:
+            input_list[k] = aux_list[i]
+            i += 1
+        elif aux_list[j] > aux_list[i]:
+            input_list[k] = aux_list[i]
+            i += 1
+        else:
+            input_list[k] = aux_list[j]
+            j += 1
+
+
+def merge_sort_helper(input_list, start, stop):
+    if start >= stop:
+        return
+    middle = start + (stop - start) // 2
+    merge_sort_helper(input_list, start, middle)
+    merge_sort_helper(input_list, middle + 1, stop)
+
+    merge(input_list, start, middle, stop)
+
+
+def merge_sort(input_list):
+    merge_sort_helper(input_list, 0, len(input_list) - 1)
+
+
+def quick_sort(input_list):
+    quick_sort_helper(input_list, 0, len(input_list) - 1)
+
+
+def quick_sort_helper(input_list, start, stop):
+    if start < stop:
+        p = partition(input_list, start, stop)
+        quick_sort_helper(input_list, start, p - 1)
+        quick_sort_helper(input_list, p + 1, stop)
---
+++ HW/HW6/time_sorts.py
@@ -0,0 +1,50 @@
+# Mark Boady
+# Drexel University
+# CS 260
+
+# This is just a starting point.
+# It times the first two sorts.
+
+# Extend this code to handle the 4 new sorts.
+# Some sorts will take to long and you will
+# have to stop them earlier than others.
+
+# Time Two Sorts
+import random
+import timeit
+
+
+# Generate a Random Array of Numbers to sort
+def rand_array(size):
+    return [random.randint(-2 * size, 2 * size) for n in range(0, size)]
+
+
+# Test the given sort on a random array of size given.
+def test_a_sort(sort_name, size):
+    if sort_name == "heapsort":
+        setup = "from heap import heapsort\n"
+    else:
+        setup = "from sort_code import " + sort_name + "\n"
+    setup += "from __main__ import rand_array\n"
+    setup += "size=" + str(size) + "\n"
+    setup += "A=rand_array(size)\n"
+    test_code = sort_name + "(A.copy())"
+    # You might need to change these parameters
+    t = timeit.timeit(test_code, setup, number=100)
+    return t
+
+
+if __name__ == "__main__":
+    # Print out the results.
+    print("Size | Built In    |    Gnome    |  BubbleSort  | InsertionSort |  MergeSort  |  QuickSort |  Heap Sort  |")
+    for x in range(0, 9):
+        rowTemplate = "{:4d} | {:11.5} | {:11.5f} | {:11.5f}  |  {:11.5f}  | {:11.5f} | {:11.5f}| {:11.5f} |"
+        built = test_a_sort("builtin", 2 ** x)
+        gnome = test_a_sort("gnome", 2 ** x)
+        bubble = test_a_sort("bubble_sort", 2 ** x)
+        insertion = test_a_sort("insertion_sort", 2 ** x)
+        merge = test_a_sort("merge_sort", 2 ** x)
+        quick = test_a_sort("quick_sort", 2 ** x)
+        heap = test_a_sort("heapsort", 2 ** x)
+
+        print(rowTemplate.format(2 ** x, built, gnome, bubble, insertion, merge, quick, heap))
