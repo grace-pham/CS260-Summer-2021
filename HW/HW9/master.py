51a4d0ff1c72479444797e5c11f22f38e85cde2a
Grace Pham <gracepham208@gmail.com>
2021-08-27 17:15:50 -0400

    fix: Edit theta_bound

--- HW/HW9/master.py
+++ HW/HW9/master.py
@@ -14,9 +14,15 @@ def master_theorem():
     if c > x:
         theta_bound = f"T(n)=Theta(n^{c})"
     elif c < x:
-        theta_bound = f"T(n)=Theta(n^{x})"  # f"T(n)=Theta(n^{x} log2(n)^{y})"
+        if y != 0:
+            theta_bound = f"T(n)=Theta(n^{x} log2(n)^{y})"
+        else:
+            theta_bound = f"T(n)=Theta(n^{x})"
     else:
-        theta_bound = f"T(n)=Theta(n^{c} log2(n)^{y + 1})"
+        if y != -1:
+            theta_bound = f"T(n)=Theta(n^{c} log2(n)^{y + 1})"
+        else:
+            theta_bound = f"T(n)=Theta(n^{c})"
     return theta_bound
 
 
