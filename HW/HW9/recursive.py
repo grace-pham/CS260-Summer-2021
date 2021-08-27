2379eaae234baf649210777486abe27853dace3f
Grace Pham <gracepham208@gmail.com>
2021-08-27 12:15:21 -0400

    feat: Upload recursive py file

---
+++ HW/HW9/recursive.py
@@ -0,0 +1,18 @@
+import math
+
+a = int(input("Enter Value for a:"))
+b = int(input("Enter Value for b:"))
+x = int(input("Enter Value for x:"))
+y = int(input("Enter Value for y:"))
+
+
+def T(n):
+    if n <= 1:
+        return 1
+    else:
+        return a * T(math.floor(n / b)) + (n ** x) * math.ceil(math.log2(n) ** y)
+
+
+if __name__ == "__main__":
+    n = int(input("Enter Value for n:"))
+    print(T(n))
