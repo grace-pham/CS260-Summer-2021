3cf821c4afcdac44275d0a3722b73e9df414bfcc
Grace Pham <gracepham208@gmail.com>
2021-08-27 14:56:25 -0400

    fix: Add master_theorem function

---
+++ HW/HW9/master.py
@@ -0,0 +1,24 @@
+import math
+
+a = int(input("Enter Value for a:"))
+b = int(input("Enter Value for b:"))
+x = int(input("Enter Value for x:"))
+y = int(input("Enter Value for y:"))
+
+
+def master_theorem():
+    c = int(math.log(a, b))
+    if c > x:
+        theta_bound = f"T(n)=Theta(n^{c})"
+    elif c < x:
+        theta_bound = f"T(n)=Theta(n^{x})"
+    else:
+        theta_bound = f"T(n)=Theta(n^{x} log2(n)^{y + 1}"
+    return theta_bound
+
+
+if __name__ == "__main__":
+    print("Automated Master Theorem")
+    print("Enter Formula T(n)=a*T(n/b)+n^x*(log2(n)^y)")
+    print(master_theorem())
+
